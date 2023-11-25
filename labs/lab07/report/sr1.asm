b2e29d8acea6d235b134a9cfb8c3673f5b56ce99
Лазарев Даниил <anarchy1928@mail.ru>
2023-11-25 23:53:34 +0300

    feat(main): add files lab-7

---
+++ labs/lab07/report/sr1.asm
@@ -0,0 +1,41 @@
+%include 'in_out.asm'
+section .data
+msg2 db "Наименьшее число: ",0h
+A dd '24'
+B dd '98'
+C dd '15'
+section .bss
+min resb 10
+section .text
+global _start
+_start:
+mov eax,B
+call atoi
+mov [B],eax
+; ---------- Записываем 'A' в переменную 'min'
+mov ecx,[A] ; 'ecx = A'
+mov [min],ecx ; 'min = A'
+; ---------- Сравниваем 'A' и 'С' (как символы)
+cmp ecx,[C] ; Сравниваем 'A' и 'С'
+jl check_B ; если 'A<C', то переход на метку 'check_B',
+mov ecx,[C] ; иначе 'ecx = C'
+mov [min],ecx ; 'min = C'
+; ---------- Преобразование 'min(A,C)' из символа в число
+check_B:
+mov eax,min
+call atoi ; Вызов подпрограммы перевода символа в число
+mov [min],eax ; запись преобразованного числа в `min`
+; ---------- Сравниваем 'min(A,C)' и 'B' (как числа)
+mov ecx,[min]
+cmp ecx,[B] ; Сравниваем 'min(A,C)' и 'B'
+jl fin ; если 'min(A,C)<B', то переход на 'fin',
+mov ecx,[B] ; иначе 'ecx = B'
+mov [min],ecx
+; ---------- Вывод результата
+fin:
+mov eax, msg2
+call sprint ; 
+mov eax,[min]
+call iprintLF ;
+call quit ; Выход
+
