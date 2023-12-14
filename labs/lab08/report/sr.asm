101fc38c4729b4b13c12ddf4fded2d9d1eb5fc93
Лазарев Даниил <anarchy1928@mail.ru>
2023-12-14 17:28:58 +0300

    feat(main): add files lab-9

--- labs/lab08/report/sr.asm
+++ labs/lab08/report/sr.asm
@@ -1,36 +1,37 @@
 %include 'in_out.asm'
 SECTION .data
-msg db "Результат: ",0
-msg2 db "Функция: f(x)=10x-4"
+msg1: DB 'Функция: f(x)=10х-4 ',0
+msg2: DB 'Результат: ',0
+SECTION .bss
+x: RESB 80
+res: RESB 80
 SECTION .text
-global _start
+GLOBAL _start
 _start:
-pop ecx ; Извлекаем из стека в ecx количество
-; аргументов (первое значение в стеке)
-pop edx ; Извлекаем из стека в edx имя программы
-; (второе значение в стеке)
-sub ecx,1 ; Уменьшаем ecx на 1 (количество
-; аргументов без названия программы)
-mov esi, 0 ; Используем esi для хранения
-; промежуточных сумм
+mov eax, msg1
+call sprintLF
+pop ecx ; загружаем количество значений x
+pop edx ; загружаем адрес первого значения x
+sub ecx,1
+mov esi, 0 ; обнуляем сумму значений f(x)
 next:
-cmp ecx,0h ; проверяем, есть ли еще аргументы
-jz _end ; если аргументов нет выходим из цикла
-; (переход на метку `_end`)
-pop eax ; иначе извлекаем следующий аргумент из стека
-call atoi ; преобразуем символ в число
-mov edx, 10
-mul edx
-sub eax, 4
-add esi,eax ; добавляем к промежуточной сумме
-; след. аргумент esi=esi+eax
-loop next ; переход к обработке следующего аргумента
+cmp ecx, 0 ; проверяем, есть ли еще значения x
+jz _end ; если нет, переходим к завершению программы
+pop eax ; загружаем очередное значение x
+call atoi ; преобразуем значение x в число
+call _calcul_f ; вызываем подпрограмму для вычисления f(x)
+mov edi,eax
+add esi, eax
+loop next
 _end:
 mov eax, msg2
-call sprintLF
-mov eax, msg ; вывод сообщения "Результат: "
 call sprint
-mov eax, esi ; записываем сумму в регистр eax
-call iprintLF ; печать результата
-call quit ; завершение программы
+mov eax, esi
+call iprintLF
+call quit
+_calcul_f:
+mov eax
+mul eax 
+add ebx, -4
+ret
 
